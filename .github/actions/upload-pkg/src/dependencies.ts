1e312ddbf355306daf6f3d6d0ae6b78036e18cba
nalchevanidze <d.nalchevanidze@gmail.com>
2022-03-25 00:09:23 +0100

    package

--- .github/actions/upload-pkg/src/dependencies.ts
+++ .github/actions/upload-pkg/src/dependencies.ts
@@ -28,6 +28,24 @@ const rules: Record<string, [string, string]> = {
   containers: ["0.4.2.1", "0.7"],
   megaparsec: ["7.0.0", "10.0.0"],
   mtl: ["2.0", "3.0"],
+  scientific: ["0.3.6.2", "0.4"],
+  text: ["1.2.3.0", "1.3"],
+  transformers: ["0.3", "0.6"],
+  ["template-haskell"]: ["2.0", "3.0"],
+  ["th-lift-instances"]: ["0.1.1", "0.3"],
+  ["unordered-containers"]: ["0.2.8.0", "0.3"],
+  ["unliftio-core"]: ["0.0.1", "0.4"],
+  vector: ["0.12.0.1", "0.13"],
+  websockets: ["0.11", "1.0"],
+  hashable: ["1.0.0", "2.0"],
+  relude: ["0.3.0", "2.0"],
+  directory: ["1.0", "2.0"],
+  uuid: ["1.0", "1.4"],
+  filepath: ["1.1", "1.5"],
+  tasty: ["0.1", "1.5"],
+  ["tasty-hunit"]: ["0.1", "1.0"],
+  ["optparse-applicative"]: ["0.12", "0.18"],
+  prettyprinter: ["1.2", "2.0"],
 };
 
 const withRule = (name: string, [min, max]: [string, string]) => [
@@ -42,7 +60,10 @@ const withRule = (name: string, [min, max]: [string, string]) => [
 const updateDependency =
   ({ bounds }: Config) =>
   ([name, ...args]: string[]): string[] => {
-    if (name.startsWith(projectPefix) && args.length) {
+    if (name.startsWith(projectPefix)) {
+      if (!args.length) {
+        return [name, ...args];
+      }
       return withRule(name, bounds);
     }
     const rule = rules[name];
@@ -50,6 +71,8 @@ const updateDependency =
     if (rule) {
       return withRule(name, rule);
     }
+
+    console.warn("unknown package:", name);
 
     return [name, ...args];
   };
